2445774a81613c45ed5ae8cfd079ee40e0157e43
hilkiasmoraes <hilkias.moraes@gmail.com>
2025-04-26 11:21:03 -0300

    feat: Crude Java Spring Boot

---
+++ src/main/java/com/senai/escola/model/Aluno.java
@@ -0,0 +1,52 @@
+package com.senai.escola.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long idAluno;
+
+    private String nomeAluno;
+    private String emailAluno;
+    private String telefoneAluno;
+
+    // Getters e Setters
+    public Long getIdAluno() {
+        return idAluno;
+    }
+
+    public void setIdAluno(Long idAluno) {
+        this.idAluno = idAluno;
+    }
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+
+    public String getEmailAluno() {
+        return emailAluno;
+    }
+
+    public void setEmailAluno(String emailAluno) {
+        this.emailAluno = emailAluno;
+    }
+
+    public String getTelefoneAluno() {
+        return telefoneAluno;
+    }
+
+    public void setTelefoneAluno(String telefoneAluno) {
+        this.telefoneAluno = telefoneAluno;
+    }
+}
